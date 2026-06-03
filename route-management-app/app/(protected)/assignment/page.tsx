52f14ae252313f4f1aebebf4940e36fd75d2e2e1
JasprC <jasperchua101@gmail.com>
2026-06-03 21:13:16 +0800

    feat(project): initialized Next.js & added page layouts
    Initialized the Next.js project and setup the bagic page layouts for
    dashboard, assignment, and routing tool.
    
    Also added a sidebar component to navigate between pages.
    
    Default Home Page is the login page for now.
    Accessible at http://localhost:3000 once code is run.

---
+++ route-management-app/app/(protected)/assignment/page.tsx
@@ -0,0 +1,11 @@
+export default function Assignment() {
+    return (
+        // This is a placeholder for the Assignment page.
+        // TODO: Implement the actual content of the Assignment page.
+        <main className="flex min-h-screen">
+            <h1 className="text-4xl font-bold">
+                Welcome to the Route Assignment Page.
+            </h1>
+        </main>
+    );
+}
---
+++ route-management-app/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+        // This is a placeholder for the Dashboard page.
+        // TODO: Implement the actual content of the Dashboard page.
+        <main className="flex min-h-screen">
+            <h1 className="text-4xl font-bold">Welcome to the Dashboard.</h1>
+        </main>
+    );
+}
