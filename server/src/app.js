41c0c0bef1ae0707e86f471d04a5a40e54234f31
jrsantiagoo <jrsantiago3001@gmail.com>
2026-06-06 15:04:28 +0800

    chore: import cors

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
+// --- MIDDLEWARE ---
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
