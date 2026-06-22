eb8d5f1abf772c4f69e21e309a509d43ad315936
JasprC <jasperchua101@gmail.com>
2026-06-22 20:26:43 +0800

    style: adjusted color

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -178,7 +178,7 @@ export default function loginPage() {
                         )}
                     </button>
 
-                    <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
+                    <h1 className="text-3xl font-bold text-foreground">
                         Welcome Back
                     </h1>
                     <p className="mt-1 text-sm text-gray-500 dark:text-slate-400">
