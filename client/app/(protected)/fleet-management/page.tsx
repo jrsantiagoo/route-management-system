155929c11360a89f11324bfad30f3305e201bd1f
JasprC <jasperchua101@gmail.com>
2026-07-14 22:24:25 +0800

    feat: added fleet management navlink to sidebar
    Added the navigation link to fleet management page to sidebar.
    Adjusted sidebar collapse button. Fleet Management page has not been
    implemented yet.

---
+++ client/app/(protected)/fleet-management/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Assignment() {
+    return (
+        <div className="flex flex-col gap-6">
+            {/* Page Header */}
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col justify-center">
+                    <h1 className="text-2xl font-bold">Fleet Management</h1>
+                    <p className="text-md text-muted-foreground">
+                        Assign routes and plan trips
+                    </p>
+                </div>
+            </div>
+
+            <div className="flex items-center justify-between"></div>
+        </div>
+    );
+}
