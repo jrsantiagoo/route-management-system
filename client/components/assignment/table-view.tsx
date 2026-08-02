d4f6a94ceafd5a9b30862b274299ccef3f04cb6e
JasprC <jasperchua101@gmail.com>
2026-08-02 23:24:50 +0800

    refactor: changed archive button color

--- client/components/assignment/table-view.tsx
+++ client/components/assignment/table-view.tsx
@@ -399,6 +399,7 @@ export default function TableView({
                                                         }
                                                         setActiveMenu(null);
                                                     },
+                                                    variant: "danger",
                                                 },
                                             ]}
                                         />
--- client/components/fleet-management/fleet-table.tsx
+++ client/components/fleet-management/fleet-table.tsx
@@ -366,6 +366,7 @@ export default function FleetTable({
                                                         }
                                                         setActiveMenu(null);
                                                     },
+                                                    variant: "danger",
                                                 },
                                             ]}
                                         />
